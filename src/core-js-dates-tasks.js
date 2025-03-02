fb530b0e886d969b66ea4ffdcdca349af6c43ef2
Andrey Zharkikh <orbital.logistics@yandex.ru>
2025-03-02 17:12:35 +0300

    feat: 4 tests passing

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -70,8 +70,12 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  do {
+    date.setTime(date.getTime() + 86_400_000);
+  } while (date.getDay() !== 5);
+
+  return date;
 }
 
 /**
